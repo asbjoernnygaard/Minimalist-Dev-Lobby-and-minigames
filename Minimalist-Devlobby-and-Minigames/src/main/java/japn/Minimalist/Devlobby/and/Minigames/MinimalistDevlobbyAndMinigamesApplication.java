a50a68188082b106a448d56a436c45c47470229e
Salt Dev <salt-dev@salt-devs-MacBook-Pro-5.local>
2022-10-25 15:15:57 +0200

    Project initialized

---
+++ Minimalist-Devlobby-and-Minigames/src/main/java/japn/Minimalist/Devlobby/and/Minigames/MinimalistDevlobbyAndMinigamesApplication.java
@@ -0,0 +1,13 @@
+package japn.Minimalist.Devlobby.and.Minigames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalistDevlobbyAndMinigamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimalistDevlobbyAndMinigamesApplication.class, args);
+	}
+
+}
---
+++ Minimalist-Devlobby-and-Minigames/src/test/java/japn/Minimalist/Devlobby/and/Minigames/MinimalistDevlobbyAndMinigamesApplicationTests.java
@@ -0,0 +1,13 @@
+package japn.Minimalist.Devlobby.and.Minigames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalistDevlobbyAndMinigamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
